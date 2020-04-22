30db1a65f99cd2a183e9a603480045880af71ef0
brojec <brojec@gmail.com>
2020-04-22 13:49:47 -0500

    basic UI done, search on type

--- webroot/js/react.js
+++ webroot/js/react.js
@@ -8,16 +8,57 @@ function Header(props){
 
 function SearchBar(props){
     return(
-        <input type="text" placeholder={props.placeholder} required={props.required} />
+        <input class="form-control" type="text" placeholder={props.placeholderText} value={props.value} onChange={props.handleInput}
+            required={props.required} />
     )
 }
 
 function Submit(props){
     return(
-        <input type="submit" value={props.value} />
+        <input class="form-control" type="submit" value={props.value} />
     )
 }
 
+function SearchForm(props){
+    return(
+        <form onSubmit={props.handleSubmit}>
+            <div class="mx-1 row">
+                <div class="col-9">
+                    <SearchBar required={props.required} placeholderText={props.placeholderText}
+                        handleInput={props.handleInput} value={props.value}/>
+                </div>
+                <div class="col-3"><Submit value={props.submitButtonText} /></div>
+            </div>  
+        </form>    
+    )
+}
+
+function Country(props){
+    return(
+        <div class="card p-2 m-3">
+            <h3>{props.name}</h3>
+            <div class="row">
+                <div class="col-6">
+                    <img src={props.flag} width="100%"/>
+                </div>
+                <div class="col-6">
+                    <div><strong>population:</strong> {Number(props.population).toLocaleString()}</div>
+                    <div><strong>region:</strong> {props.region}</div>
+                    <div class="indent-1"><strong>subregion:</strong> {props.subregion}</div>
+                    <div><strong>2 letter code</strong> {props.alpha2Code}</div>
+                    <div><strong>3 letter code</strong> {props.alpha3Code}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function CountryList(props){
+    const country_list=props.countries.map((country)=>
+        <Country key={country.name} {...country} />
+    );
+    return country_list;
+}
 
 function Region(props){
     //let total=0;
@@ -34,26 +75,6 @@ function Region(props){
     )
 }
 
-function Country(props){
-    return(
-        <div class="card p-2 m-2">
-            <h3>{props.name}</h3>
-            <div class="row">
-                <div class="col-6">
-                    <img src={props.flag} width="100%"/>
-                </div>
-                <div class="col-6">
-                    <div>population: {props.population}</div>
-                    <div>region: {props.region}</div>
-                    <div class="indent-1">subregion: {props.subregion}</div>
-                    <div>2 letter code: {props.alpha2Code}</div>
-                    <div>3 letter code: {props.alpha3Code}</div>
-                </div>
-            </div>
-        </div>
-    )
-}
-
 function SubRegion(props){
     return(
         <li><span>{props.name}: {props.value}</span></li>
@@ -61,71 +82,56 @@ function SubRegion(props){
 }
 
 class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInput = this.handleInput.bind(this);
+        this.state = {searchTerm:'', countries:[]};
+    }
+    handleInput(event){
+        this.setState({searchTerm:event.target.value})
+        this.handleSubmit(event);
+    }
+    handleSubmit(event){
+        const formData = new FormData();
+        formData.append('searchTerm',this.state.searchTerm);
+        fetch('/api/',{
+            method: "POST",
+            body: formData
+        }).then((response)=> response.json()).then((data)=>{
+            this.setState({countries: data.response});
+        })
+        event.preventDefault();
+    }
     render(){
-        const countries=[
-            {
-                "flag": "https://restcountries.eu/data/umi.svg",
-                "name": "United States Minor Outlying Islands",
-                "alpha2Code": "UM",
-                "alpha3Code": "UMI",
-                "region": "Americas",
-                "subregion": "Northern America",
-                "population": 300
-            },
-            {
-                "flag": "https://restcountries.eu/data/tza.svg",
-                "name": "Tanzania, United Republic of",
-                "alpha2Code": "TZ",
-                "alpha3Code": "TZA",
-                "region": "Africa",
-                "subregion": "Eastern Africa",
-                "population": 55155000
-            },
-            {
-                "flag": "https://restcountries.eu/data/are.svg",
-                "name": "United Arab Emirates",
-                "alpha2Code": "AE",
-                "alpha3Code": "ARE",
-                "region": "Asia",
-                "subregion": "Western Asia",
-                "population": 9856000
-            },
-            {
-                "flag": "https://restcountries.eu/data/gbr.svg",
-                "name": "United Kingdom of Great Britain and Northern Ireland",
-                "alpha2Code": "GB",
-                "alpha3Code": "GBR",
-                "region": "Europe",
-                "subregion": "Northern Europe",
-                "population": 65110000
-            },
-            {
-                "flag": "https://restcountries.eu/data/usa.svg",
-                "name": "United States of America",
-                "alpha2Code": "US",
-                "alpha3Code": "USA",
-                "region": "Americas",
-                "subregion": "Northern America",
-                "population": 323947000
-            },
-            {
-                "flag": "https://restcountries.eu/data/mex.svg",
-                "name": "Mexico",
-                "alpha2Code": "MX",
-                "alpha3Code": "MEX",
-                "region": "Americas",
-                "subregion": "Central America",
-                "population": 122273473
+        const formConfig={
+            placeholderText: "Enter country name (full or partial), or 2 or 3 letter ISO-3166 code",
+            submitButtonText: "Search",
+            required: true,
+            handleSubmit: this.handleSubmit,
+            handleInput: this.handleInput,
+            value: this.state.value
+        }
+        let regionCounts={};
+        this.state.countries.forEach((country)=> {
+            if(!regionCounts[country.region]){
+                regionCounts[country.region]={};
             }
-        ]
-        const countries_render = countries.map((country) => 
-            <Country key={country.name} {...country} />
+            if(!regionCounts[country.region][country.subregion]){
+                regionCounts[country.region][country.subregion]=0;
+            }
+            regionCounts[country.region][country.subregion]++;
+        }
         );
         return(
-            <div class="container-md">
-                <Header value="Country Search (React)" />
-                <div>{countries_render}</div>
-                <Region name="Americas" total={3} subRegions={[{name:"Northern America", value:2},{name:"Central America",value:1}]} />
+            <div class="container">
+                <Header value="Country Search" />
+                <SearchForm {...formConfig} />
+                <CountryList countries={this.state.countries} />
+                <div class="card bg-light mb-5 p-1">
+                    <h3><strong>Region totals</strong></h3>
+                    <Region name="Americas" total={3} subRegions={[{name:"Northern America", value:2},{name:"Central America",value:1}]} />
+                </div>
             </div>
         )
     }
